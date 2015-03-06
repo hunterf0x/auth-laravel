a36ca589d971e6830ba497f18d43a09c5f35b9e6
Hector Varas <king.cold@gmail.com>
2015-03-06 00:15:10 -0300

    agrego template con bootstrap por CDN y creo js para realizar validacion por ajax

--- app/views/pages/login.blade.php
+++ app/views/pages/login.blade.php
@@ -1,30 +1,73 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Authenticate with Laravel 4.2</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-<div class="container">
-    {{ Form::open(['url' => 'login']) }}
+<div class="row-fluid">
+    <div class="login-box">
+        <div class="icons">
+            <a href="{{url('/')}}"><i class="halflings-icon home"></i></a>
+            <a href="#"><i class="halflings-icon cog"></i></a>
+        </div>
+        <h2>Autenticación</h2>
+        {{ Form::open(['url' => 'login', 'class'=>'form-horizontal ajaxFormLogin', 'method'=>'POST']) }}
 
-    @if(Session::has('error_message'))
-        {{ Session::get('error_message') }}
-    @endif
-
-    <h2>Log in</h2>
-
-    {{ Form::label('username', 'Username') }}
-    {{ Form::text('username') }}
-
-    {{ Form::label('password', 'Password') }}
-    {{ Form::password('password') }}
+        <div class="validacion" style="width: 80%;padding-left: 10%"></div>
 
 
+        <fieldset>
 
-    {{ Form::submit('Log in', ['class' => 'btn btn-primary btn-block']) }}
+            <div class="input-prepend" title="Username">
+                <span class="add-on"><i class="halflings-icon user"></i></span>
+                {{ Form::text('username',"", ['class'=>'input-large span10', 'id'=>'username', 'placeholder'=>'username']) }}
 
-    {{ Form::close() }}
-</div>
+            </div>
+            <div class="clearfix"></div>
+
+            <div class="input-prepend" title="Password">
+                <span class="add-on"><i class="halflings-icon lock"></i></span>
+                {{ Form::password('password',['class'=>'input-large span10', 'id'=>'password', 'placeholder'=>'Password']) }}
+
+            </div>
+            <div class="clearfix"></div>
+
+            <label class="remember" for="remember">{{ Form::checkbox('remember', true) }}Recuerdame</label>
+
+            <div class="button-login">
+                {{ Form::submit('Acceder', ['class' => 'btn btn-primary']) }}
+
+            </div>
+            <div class="clearfix"></div>
+        </fieldset>
+        {{Form::close()}}
+        <hr>
+        <p><a href="#">recupera tu password</a>
+        </p>
+    </div><!--/span-->
+</div><!--/row-->
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+{{HTML::script("assets/js/functions.js")}}
 </body>
 </html>
---
+++ public/assets/js/functions.js
@@ -0,0 +1,75 @@
+/**
+ * Created by hvaras on 25-02-15.
+ */
+$(function() {
+
+    loadFormAjax();
+
+    loadUploaders();
+
+});
+
+
+function loadFormAjax() {
+    //Formulario Login submit con ajax
+    $(document).on("submit", ".ajaxFormLogin", function () {
+        var form = this;
+        if (!form.submitting) {
+            form.submitting = true;
+            var boton = $(this).find('button[type="submit"]');
+            boton.attr("disabled", "disabled");
+            if (boton.attr('data-loading-text'))
+                boton.button('loading');
+
+            $.ajax({
+                url: form.action,
+                data: $(form).serialize(),
+                type: form.method,
+                dataType: "json",
+                success: function (response) {
+                    console.log(response);
+                    if (response.validacion) {
+                        if (response.redirect) {
+
+                            window.location = response.redirect;
+                        } else {
+
+                            $(form).html(response.mensaje);
+                        }
+                    }
+                    else {
+
+
+                        var $validacion = $(form).find(".validacion");
+                        var errors ='';
+                        $.each(response.errores, function(index, value) {
+                            errors = errors+'<div class="alert alert-block" role="alert">' +
+                            '<button type="button" class="close" data-dismiss="alert">×</button>' + value +
+
+                            '</div>';
+                        });
+                        $validacion.html(errors);
+                        $('html, body').animate({
+                            scrollTop: $validacion.offset().top-10
+                        });
+                    }
+                },
+                complete: function () {
+                    boton.removeAttr("disabled").button('reset');
+                    //$(ajaxLoader).hide();
+                    form.submitting = false;
+                }
+            });
+        }
+        return false;
+    });
+
+
+
+
+}
+
+
+
+
+
