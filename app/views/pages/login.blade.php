71166168a14b1519d4930832a2c892eaa3cacd24
Hector Varas <king.cold@gmail.com>
2015-03-06 00:29:35 -0300

    se corrige path a css

--- app/views/pages/login.blade.php
+++ app/views/pages/login.blade.php
@@ -18,8 +18,9 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 
-    {{HTML::style("assets/css/style.css")}}
+
     <![endif]-->
+    {{HTML::style("assets/css/style.css")}}
 </head>
 <body>
 <div class="row-fluid">
