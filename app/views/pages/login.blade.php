d00bf9655d4f57cb45f0a86e15b754467c04e752
Hector Varas <king.cold@gmail.com>
2015-03-06 00:26:44 -0300

    se agrega css para darle look & feel al login

--- app/views/pages/login.blade.php
+++ app/views/pages/login.blade.php
@@ -17,6 +17,8 @@
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+    {{HTML::style("assets/css/style.css")}}
     <![endif]-->
 </head>
 <body>
@@ -58,7 +60,7 @@
         </fieldset>
         {{Form::close()}}
         <hr>
-        <p><a href="#">recupera tu password</a>
+
         </p>
     </div><!--/span-->
 </div><!--/row-->
--- public/assets/js/functions.js
+++ public/assets/js/functions.js
@@ -5,7 +5,7 @@ $(function() {
 
     loadFormAjax();
 
-    loadUploaders();
+
 
 });
 
